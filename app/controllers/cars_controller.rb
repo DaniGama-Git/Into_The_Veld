60067f9902279d084529cbd5551b85e1f9ab42a3
Tassos Psimitis <tassospsimitis@gmail.com>
2023-05-30 12:33:24 +0200

    Made the cars controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,19 +1,50 @@
 class CarsController < ApplicationController
+  before_action :set_car, only: %i[show edit update destroy]
+
+  def index
+    @cars = Car.all
+  end
+
   def show
   end
 
   def new
+    @car = Car.new
   end
 
   def create
+    @car = Car.new(cars_params)
+
+    if @car.save
+      redirect_to @car, notice: "#{@car.title} has been successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @car.update(car_params)
+      redirect_to @car, notice: "#{@car.title} has been successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @car.destroy
+    redirect_to cars_url, notice: "#{@car.title} has been successfully destroyed"
+  end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
+
+  def car_params
+    params.require(:car).permit(:title, :description, :price, :start_date, :end_date, :make, :model, :accomodation, :cooking, :seats, :consumption)
   end
 end
