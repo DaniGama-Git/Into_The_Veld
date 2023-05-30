1f594dc9bb56a0d2411d75172d80bf4df2aa7518
Tassos Psimitis <tassospsimitis@gmail.com>
2023-05-30 17:34:39 +0200

    Did the Owners journey, Pundit for the car and added photo to simple form and car params

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,19 +2,22 @@ class CarsController < ApplicationController
   before_action :set_car, only: %i[show edit update destroy]
 
   def index
-    @cars = Car.all
+    @cars = policy_scope(Car)
   end
 
   def show
+    authorize @car
   end
 
   def new
     @car = Car.new
+    authorize @car
   end
 
   def create
-    @car = Car.new(cars_params)
-
+    @car = Car.new(car_params)
+    @car.user_id = current_user.id
+    authorize @car
     if @car.save
       redirect_to @car, notice: "#{@car.title} has been successfully created."
     else
@@ -23,17 +26,20 @@ class CarsController < ApplicationController
   end
 
   def edit
+    authorize @car
   end
 
   def update
+    authorize @car
     if @car.update(car_params)
-      redirect_to @car, notice: "#{@car.title} has been successfully."
+      redirect_to @car, notice: "#{@car.title} has been successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
+    authorize @car
     @car.destroy
     redirect_to cars_url, notice: "#{@car.title} has been successfully destroyed"
   end
@@ -45,6 +51,6 @@ class CarsController < ApplicationController
   end
 
   def car_params
-    params.require(:car).permit(:title, :description, :price, :start_date, :end_date, :make, :model, :accomodation, :cooking, :seats, :consumption)
+    params.require(:car).permit(:title, :description, :price, :start_date, :end_date, :make, :model, :accomodation, :cooking, :seats, :consumption, :photo)
   end
 end
