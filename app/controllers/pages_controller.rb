1e0d0e875ab12663615528be95be29f6c080da65
Ben Malandrinos <bmalandrinos@gmail.com>
2023-06-02 16:58:22 +0200

    finished styling for dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,5 +9,8 @@ class PagesController < ApplicationController
     @bookings = Booking.where(user: current_user)
     @cars = Car.where(user: current_user)
     @my_car_rentals = Booking.all.select{ |booking| booking.car.user == current_user }
+    @requests = @my_car_rentals.select{ |rental| rental.status == "pending" }
+    @upcoming = @my_car_rentals.select{ |rental| rental.status == "accepted" && rental.start_date > Date.today }
+    @history = @my_car_rentals.select{ |rental| rental.status == "accepted" && rental.end_date < Date.today }
   end
 end
