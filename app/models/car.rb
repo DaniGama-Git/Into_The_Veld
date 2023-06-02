4b8ee347f86f2b9d5367608b8aa1dfafb3dedb12
Dani Gama <130760066+DaniGama-Git@users.noreply.github.com>
2023-06-02 16:08:29 +0200

    Changed banner, and added search function

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,13 @@
+require "pg_search"
 class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many_attached :photos
+
+  include PgSearch::Model
+  pg_search_scope :search_by_make_and_model,
+    against: [ :make, :model ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
